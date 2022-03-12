0c43c91b11a9d48399427d607e4500d71c2457a2
Miruna Mihaita <miruna.m@csscweb.com>
2022-03-12 11:21:27 +0200

    keyboard event and animation function

--- examples/example.js
+++ examples/example.js
@@ -48,6 +48,29 @@ let houseKeeper1 = new HouseKeeper("Jane", 12, ['hotel1', 'hotel2'])
 console.log(houseKeeper1.firstName)
 
 
+
+// let bellBoy1 = new BellBoy("Tom", 19, true, ["eng", 'fr'])
+// let bellBoy2 = new BellBoy("John", 18, false, ["eng", 'de'])
+
+// console.log(bellBoy1.age)
+
+
+
+// const bellBoy1 = {
+//     name: "Tom",
+//     age: 19,
+//     experience: true,
+//     languages: ["English", "French"],
+//     moveSuitcase: function (){
+//         alert('May I move your suitcase?');
+//         pickUpSuitcase();
+//         move();
+//     }
+// }
+
+// bellBoy1.moveSuitcase();
+
+
 // CONSTRUCTOR FUNCTION
 
 function BellBoy (name, age, experience, languages){
@@ -55,9 +78,30 @@ function BellBoy (name, age, experience, languages){
     this.age = age;
     this.experience = experience;
     this.languages = languages;
+    this.moveSuitcase = function(){
+        console.log('the method works')
+    }
 }
 
 let bellBoy1 = new BellBoy("Tom", 19, true, ["eng", 'fr'])
-let bellBoy2 = new BellBoy("John", 18, false, ["eng", 'de'])
 
-console.log(bellBoy1.age)
+bellBoy1.moveSuitcase();
+
+// 1. adaugati metoda moveSuitcase la functia constructor
+// 2. creati un obiect nou cu ajutorul functiei constructor
+// 3. apelati metoda din noul obiect
+
+
+//CONSTRUCTOR FUNCTION
+
+function Audio(fileLocation){
+    this.fileLocation = fileLocation;
+    this.play = function(){
+        //Find the audio on the hardware
+        //Check location to see if file exists
+        //Play the file in fileLocation
+    }
+}
+
+let tom2 = new Audio("sounds/tom-2.mp3");
+tom2.play();
